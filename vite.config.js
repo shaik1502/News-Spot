e61d376e5c621834503369c0b770ba3a31b57d8d
Sameer Shaik <114245858+shaik1502@users.noreply.github.com>
2025-01-28 02:55:30 +0530

    Updated base URL for renamed repo

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/news-spot/", // Update with your new repo name
   plugins: [react()],
-  base:"/news-spot"
-})
- 
+});
