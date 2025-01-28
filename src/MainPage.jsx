84a60a52950324dce313cfd65f98b80f998b5585
Sameer Shaik <114245858+shaik1502@users.noreply.github.com>
2025-01-28 23:59:23 +0530

    changed API

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import CardTab from "./CardTab";
-import SearchBox from "./searchbox";
+import SearchBox from "./SearchBox";
 import axios from "axios";
 
 function MainPage() {
   const [articles, setArticles] = useState([]);
   const [query, setQuery] = useState("mobiles");
 
+  // const API_KEY = import.meta.env.VITE_API_KEY1;
+  // const url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`;
 
-  const API_KEY = "da17f57c22034e9c9b442ae0b3c6dffb";
-  const url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`;
+  const API_KEY = import.meta.env.VITE_API_KEY2;
+  const url =
+    `https://gnews.io/api/v4/search?q=${query}&lang=en&max=10&apikey=` +
+    API_KEY;
 
- 
-  // Function to fetch news based on query
   async function fetchNews() {
-    const url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`;
     try {
       const { data } = await axios.get(url);
       setArticles(data.articles);
@@ -22,21 +23,18 @@ function MainPage() {
       console.error("Error fetching news:", error);
     }
   }
-
-  // Fetch news whenever `query` changes
   useEffect(() => {
     fetchNews();
   }, [query]);
 
+  console.log(articles);
+  const handleSearch = (searchTerm) => {
+    setQuery(searchTerm); // Update the query when a new search term is provided
+  };
 
-
-     const handleSearch = (searchTerm) => {
-       setQuery(searchTerm); // Update the query when a new search term is provided
-     };
-  
   return (
     <div>
-      <SearchBox onSearch={handleSearch}/>
+      <SearchBox onSearch={handleSearch} />
       <CardTab articles={articles} />
     </div>
   );
--- src/cardstyle.jsx
+++ src/cardstyle.jsx
@@ -14,6 +14,7 @@ import { Favorite, Share } from "@mui/icons-material";
 import { red } from "@mui/material/colors";
 
 export default function RecipeReviewCard({ article }) {
+
   return (
     <Card
       sx={{
@@ -54,7 +55,7 @@ export default function RecipeReviewCard({ article }) {
       <CardMedia
         component="img"
         height="200"
-        image={article.urlToImage || "https://via.placeholder.com/200x200"} // Placeholder image if no image is available
+        image={article.image || "https://source.unsplash.com/random"} // Fallback image in case the article doesn't have an image
         alt={article.title}
         sx={{
           borderRadius: "8px",
